7b0febd220a23d8108426821aa478421fda3ed28
YOUR NAMEcls <faiza.prism@gmail.com>
2025-03-11 14:14:41 +0500

    RAK opportunities page dev

---
+++ app/ras-al-khaimah-opportunities/page.js
@@ -0,0 +1,19 @@
+import PageContent from "./pageContent";
+
+export async function generateMetadata() {
+  return {
+    title: "GO Property Management Services | Trusted Real Estate Agency",
+    description:
+      "GO, is your trusted real estate agency specializing in property management. We maximize your property's value with tailored solutions and exceptional service.",
+    alternates: {
+      canonical:
+        "https://newedge-realty-next.vercel.app/real-estate-investments-in-uae",
+    },
+  };
+}
+
+const RasAlKhaimahOpportunities = () => {
+  return <PageContent />;
+};
+
+export default RasAlKhaimahOpportunities;
---
+++ app/ras-al-khaimah-opportunities/pageContent.js
@@ -0,0 +1,66 @@
+"use client";
+// import { useState, useEffect } from "react";
+import Banner from "@/components/common/common-banner/CommonBanner";
+import AboutOpportunity from "@/components/opportunities/about-opportunity/AboutOpportunity";
+import WhyInvestSlider from "@/components/opportunities/why-invest-slider/WhyInvestSlider";
+import DevelopmentTrends from "@/components/opportunities/development-trends/DevelopmentTrends";
+import FAQSection from "@/components/home/faq-section/FAQSection";
+// api
+// import { fatchPagesContent } from "@/app/apis/commonApi";
+// data
+import { rakOpportunitiesData } from "@/lib/rakOpportunitiesData";
+
+const PageContent = () => {
+  const { about, why_invest, development_trends } = rakOpportunitiesData;
+
+  // const [pageData, setPageData] = useState({});
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // const getPageData = async () => {
+  //   try {
+  //     setIsLoading(true);
+  //     const resp = await fatchPagesContent("about");
+  //     setPageData(resp?.data);
+  //   } catch (err) {
+  //     console.log("Err: ", err);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getPageData();
+  // }, []);
+
+  return (
+    <>
+      <Banner
+        // name={pageData?.content?.banner?.title}
+        name="Ras Al Khaimah Opportunities"
+        indexpage="Home"
+        indexvisit="/"
+        activepage="Ras Al Khaimah Opportunities"
+        // bgImg={
+        //   pageData?.content?.banner?.background_image
+        //     ? {
+        //         src: `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${pageData?.content?.banner?.background_image}`,
+        //       }
+        //     : {src:"/assets/banner/aboutbanner.webp"}
+        // }
+        bgImg={{ src: "/assets/banner/aboutbanner.webp" }}
+      />
+      <AboutOpportunity aboutData={about} />
+      <WhyInvestSlider
+        whyInvestData={why_invest}
+        title="Why Invest In RAK Real Estate"
+      />
+      <DevelopmentTrends
+        devTrendsData={development_trends}
+        title="RAK Development Trends"
+      />
+      <FAQSection />
+    </>
+  );
+};
+
+export default PageContent;
