1251a723d937e0dad265c42dd9ebe32b524923f6
YOUR NAMEcls <faiza.prism@gmail.com>
2025-03-19 13:10:21 +0500

    faqs api integration

--- app/faqs/page.js
+++ app/faqs/page.js
@@ -1,21 +1,21 @@
-import { fatchPagesContent } from "@/app/apis/commonApi";
+// import { fatchPagesContent } from "@/app/apis/commonApi";
 import PageContent from "./pageContent";
 
-export async function generateMetadata() {
-  const { data } = await fatchPagesContent("faqs");
+// export async function generateMetadata() {
+//   const { data } = await fatchPagesContent("faqs");
 
-  return {
-    title:
-      data?.content?.seo?.meta_title ||
-      "FAQs | Global Opportunities Real Estate (GO Real Estate)",
-    description:
-      data?.content?.seo?.meta_description ||
-      "Find answers to common real estate questions with GO Real Estate. Explore investment insights, buying guides, and expert advice for property decisions in Dubai.",
-    alternates: {
-      canonical: "https://gogrouprealestate.vercel.app/faqs",
-    },
-  };
-}
+//   return {
+//     title:
+//       data?.content?.seo?.meta_title ||
+//       "FAQs | Global Opportunities Real Estate (GO Real Estate)",
+//     description:
+//       data?.content?.seo?.meta_description ||
+//       "Find answers to common real estate questions with GO Real Estate. Explore investment insights, buying guides, and expert advice for property decisions in Dubai.",
+//     alternates: {
+//       canonical: "https://gogrouprealestate.vercel.app/faqs",
+//     },
+//   };
+// }
 
 const Faqs = () => {
   return <PageContent />;
