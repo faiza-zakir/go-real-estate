963ee0d1ac73ab9a3988b03befa394f138117f41
YOUR NAMEcls <faiza.prism@gmail.com>
2025-03-19 13:41:16 +0500

    resolve build issue

--- app/privacy-policy/page.js
+++ app/privacy-policy/page.js
@@ -1,21 +1,21 @@
-// import { fatchPagesContent } from "@/app/apis/commonApi";
-import PageContent from "./pageContent";
+import { fatchPagesContent } from "@/app/apis/commonApi";
+import PageContent from "./PageContent";
 
-// export async function generateMetadata() {
-//   const { data } = await fatchPagesContent("privacy-policy");
+export async function generateMetadata() {
+  const { data } = await fatchPagesContent("privacy-policy");
 
-//   return {
-//     title:
-//       data?.content?.seo?.meta_title ||
-//       "Read the Privacy Policy by Global Opportunities Real Estate",
-//     description:
-//       data?.content?.seo?.meta_description ||
-//       "Read the Privacy Policy of Global Opportunities Real Estate to learn how we collect, use, and protect your personal information while using our services. Join us",
-//     alternates: {
-//       canonical: "https://gogrouprealestate.vercel.app/privacy-policy",
-//     },
-//   };
-// }
+  return {
+    title:
+      data?.content?.seo?.meta_title ||
+      "Read the Privacy Policy by Global Opportunities Real Estate",
+    description:
+      data?.content?.seo?.meta_description ||
+      "Read the Privacy Policy of Global Opportunities Real Estate to learn how we collect, use, and protect your personal information while using our services. Join us",
+    alternates: {
+      canonical: "https://gogrouprealestate.vercel.app/privacy-policy",
+    },
+  };
+}
 
 const PrivacyPolicy = () => {
   return <PageContent />;
